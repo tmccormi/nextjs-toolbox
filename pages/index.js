bbd838784329c092c010366727b71daf085ee838
Tony McCormick <285835+tmccormi@users.noreply.github.com>
2023-04-20 11:36:59 -0700

    remove JokeBlock

--- pages/index.js
+++ pages/index.js
@@ -2,25 +2,23 @@ import Head from "next/head";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
 import FeedbackForm from "@components/FeedbackForm";
-import JokeBlock from "@components/JokeBlock";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Toolbox</title>
+        <title>Complete Care PPC</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="CCPPC Web Chat PCO2" />
+        <Header title="CCPPC Web Chat POC 2" />
         <hr />
         <p className="description">
           Here's an example of a Netlify Form! When you fill this out, the
           submissions can be found in the Netlify Admin site.
         </p>
         <FeedbackForm />
-        <JokeBlock />
       </main>
       <Footer />
     </div>

e433f2d0d60b4e0dce7b7a69ce1c033ace646c18
Tony McCormick <285835+tmccormi@users.noreply.github.com>
2023-04-20 11:55:49 -0700

    Draft site page
    Just to have something product related

--- components/Header.js
+++ components/Header.js
@@ -3,24 +3,40 @@ export default function Header({ title }) {
     <>
       <h1 className="title">{title}</h1>
       <p>
-        Hi 👋! This template gives you a{" "}
-        <a href="https://nextjs.org/">Next.js</a> app with the scaffolding for{" "}
-        <a href="https://www.netlify.com/products/functions/">
-          Netlify Functions
-        </a>
-        , <a href="https://www.netlify.com/products/forms/">Forms</a>, and{" "}
-        <a href="https://docs.netlify.com/routing/redirects/">Redirects</a>. Our
-        aim was to give you the code you would need to hit the ground running
-        with a few fun features.
+        <ul>
+        <li><strong>No Age Limits</strong></li>   
+        <li><strong>No Maximum Coverage Limits</strong></li>
+        <li><strong>A-Rated Insurers</strong></li>
+        <li><strong>Same Pricing Nationwide</strong></li>
       </p>
+      <h2>An affordable national health plan that includes all of the following
+      <div class="table">
+  <div class="table-row">
+    <div class="table-cell">4 Million PCHS Physicians and Hospitals</div>
+  </div>
+  <div class="table-row">
+    <div class="table-cell">Telemedicine 24 Hrs 365 Days Board Certified Docs No-Copay</div>
+  </div>
+  <div class="table-row">
+    <div class="table-cell">Quest Labs -Any Test No Copay</div>
+  </div>
+  <div class="table-row">
+    <div class="table-cell">Grow Therapy – Mental Health In Office and Virtual - $30 Copay</div>
+  </div>
+  <div class="table-row">
+    <div class="table-cell">Lowest Cost RX with Drexi</div>
+  </div>
+  <div class="table-row">
+    <div class="table-cell">Legal Defense for Patients from Excess Bills – No Copay</div>
+  </div>
+  <div class="table-row">
+    <div class="table-cell">Dental, Vision, Hearing, DME 50-70% off</div>
+  </div>
+  <div class="table-row">
+    <div class="table-cell">Roadside Assistance</div>
+  </div>
+</div>
 
-      <p>
-        You can find the code for this project on GitHub at{" "}
-        <a href="https://github.com/netlify-templates/nextjs-toolbox">
-          https://github.com/netlify-templates/nextjs-toolbox
-        </a>
-        ! Happy coding!
-      </p>
     </>
   );
 }
